275fc2f0b5c61937e6a83286618bbdb4ac9e02a1
Ning Xuan <wee_ning_xuan_2028@mgs.sch.edu.sg>
2025-08-16 11:58:28 +0800

    added home screen

--- Subwaysurfersapp/ContentView.swift
+++ Subwaysurfersapp/ContentView.swift
@@ -6,20 +6,65 @@
 //
 
 import SwiftUI
+var currentStreak = 5
 
 struct ContentView: View {
     var body: some View {
+        
         TabView {
+            Tab("Home", systemImage: "house") {
+                
+                VStack {
+                    
+                    HStack {
+                        Text("Welcome, User!")
+                            .bold()
+                            .font(.system(size: 30))
+                        Text("Streak \(currentStreak)")
+                            .padding()
+                            .foregroundStyle(Color.red)
+                        Spacer()
+                    }
+                    
+                    Text("Current Badge:")
+                        .bold()
+                        .font(.system(size:30))
+                        .padding()
+                    
+                    Image("Badge")
+                        .resizable()
+                        .frame(width: 300 , height: 350)
+                    
+                    
+                    Button("edit...") {
+                        
+                    }
+                    .font(.system(size: 25))
+                    
+                    
+                    Button("Start") {
+                        
+                    }
+                    .bold()
+                    .padding(30)
+                    .font(.system(size:50))
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    Spacer()
+                    
+                }
+                .padding()
+                
             }
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Tab("Leaderboard", systemImage: "heart") {
+                
+            }
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
