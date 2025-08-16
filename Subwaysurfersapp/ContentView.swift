886b01abe03cb7181b76c52015aae9c96e319fd4
Ning Xuan <wee_ning_xuan_2028@mgs.sch.edu.sg>
2025-08-16 10:01:39 +0800

    :)

--- Subwaysurfersapp/ContentView.swift
+++ Subwaysurfersapp/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        TabView {
+            }
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
