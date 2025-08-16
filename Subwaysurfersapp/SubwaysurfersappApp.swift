a767bb457913a010745d4e9b36561a3f90996277
Ning Xuan <wee_ning_xuan_2028@mgs.sch.edu.sg>
2025-08-16 09:53:05 +0800

    Initial Commit

---
+++ Subwaysurfersapp/SubwaysurfersappApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubwaysurfersappApp.swift
+//  Subwaysurfersapp
+//
+//  Created by T Krobot on 16/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SubwaysurfersappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
